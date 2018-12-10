98c58bb3bb02ed92ab60887b82ebd4117a778283
Debjit Roy <deroy@eng.ucsd.edu>
2018-12-10 23:45:47 +0530

    Computer VIsion with Deep Learning practice ipynb

---
+++ scenery/dataset/Image_download.py
@@ -0,0 +1,8 @@
+from google_images_download import google_images_download   #importing the library
+import sys
+response = google_images_download.googleimagesdownload()   #class instantiation
+
+title = sys.argv[1]
+arguments = {"keywords":title,"limit":40,"print_urls":True,"chromedriver":"C:/Users/debji/ml_src/cricket/dataset/chromedriver.exe"}   #creating list of arguments
+paths = response.download(arguments)   #passing the arguments to the function
+print(paths)
